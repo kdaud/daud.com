2371b883d7403390cf0a1a2f322c777096bba8e2
Kakumirizi Daud <dkakumirizii@gmail.com>
2020-12-29 16:58:13 +0300

    commit at my github page

---
+++ algorithmforwebapp/sourcecode/student.js
@@ -0,0 +1,28 @@
+
+var names="Daud Kakumirizi";
+if(names.endsWith("izi")){
+    console.log("PRINT: "+names);
+}
+else{
+    console.log("Invalid Entry");
+}
+console.log("Hello ");
+
+
+var organisation =[
+'OpenMRS',
+'JabRef',
+'Eclipse',
+'Amref',
+'ODK',
+'OpenSuse',
+'Mozilla'
+]
+
+console.log(organisation.push(3,"Spy","Firefox"));
+organisation.forEach(x => console.log(x));
+ var x,y;
+ x=22;
+ y=5;
+ 
+ console.log("The value is : "+x*y);
